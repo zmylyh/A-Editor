9109b82b8ba6f1a4fede86c641ae8de05138cc7c
zmylyh <zmylyh@outlook.com>
2023-04-08 16:40:45 +0800

    提交内容函数

--- back-end/editor/routes/index.js
+++ back-end/editor/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const bodyParser = require('body-parser');
+const app = bodyParser.urlencoded({ extended: false });
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,3 +10,19 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 const db = require("../db/index");
+
+router.use(express.urlencoded({extended: false}))
+
+app.post('/submit', (req, res) => {
+  const htmlData = req.body.htmldata;
+  const id = req.body.id;
+  const sql = 'INSERT INTO mytable (null,id,html_data) VALUES (id,id_f,html_data);'
+
+
+  db.query(sql, htmlData, (err, result) => {
+    if (err) throw err
+    res.send('插入数据库失败');
+    // console.log('插入数据库成功');
+    res.send('插入数据库成功');
+  });
+});
