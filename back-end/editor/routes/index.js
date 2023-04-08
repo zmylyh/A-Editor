602d565b0129d76d29c5b65d09a26dea54abbb76
zmylyh <zmylyh@outlook.com>
2023-04-08 17:04:42 +0800

    改动连接池

--- back-end/editor/routes/index.js
+++ back-end/editor/routes/index.js
@@ -7,22 +7,45 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
 
-const db = require("../db/index");
 
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password: "******",
+  database: "test_db",
+});
 router.use(express.urlencoded({extended: false}))
 
 app.post('/submit', (req, res) => {
   const htmlData = req.body.htmldata;
   const id = req.body.id;
-  const sql = 'INSERT INTO mytable (null,id,html_data) VALUES (id,id_f,html_data);'
+  const sql = 'INSERT INTO html_data (null,id,html_data) VALUES (id,id_f,html_data);'
 
 
   db.query(sql, htmlData, (err, result) => {
     if (err) throw err
-    res.send('插入数据库失败');
+    return res.send({ state: 0, message: err });
     // console.log('插入数据库成功');
-    res.send('插入数据库成功');
+    return res.send({ state: 1, message: '插入数据库成功' });
   });
 });
+
+
+app.post('/update', (req, res) => {
+  const htmlData = req.body.htmldata;
+  const id = req.body.id;
+  const sql = 'UPDATE html_data set html_data = htmldata WHERE id_f = id;'
+
+
+  db.query(sql, htmlData, (err, result) => {
+    if (err) throw err
+    return res.send({ state: 0, message: err });
+    // console.log('更新成功');
+    return res.send({ state: 1, message: '更新成功' });
+  });
+});
+
+module.exports = router;
